2884a99610391d6644cb09755c1adb7d60f3918b
lachlan <lachlan.00@gmail.com>
2012-11-15 14:23:02 +1000

    updated licence information. included lastexport.py authors

--- lastexport.py
+++ lastexport.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+""" Script for exporting tracks through audioscrobbler API.
+    Usage: lastexport.py -u USER [-o OUTFILE] [-p STARTPAGE] [-s SERVER]
+    ----------------Authors----------------
+    Petr Viktorin <encukou@gmail.com>
+    Jonas Haraldsson <https://gitorious.org/~kabniel>
+    ----------------Licence----------------
+    GNU General Public License version 3
 
-"""
-Script for exporting tracks through audioscrobbler API.
-Usage: lastexport.py -u USER [-o OUTFILE] [-p STARTPAGE] [-s SERVER]
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 import urllib2, urllib, sys, time, re
--- mergeplays.py
+++ mergeplays.py
@@ -1,24 +1,26 @@
-#!/usr/bin/python
-# coding: utf-8
-#
-# Copyright (C)2011 Lachlan de Waard
-#
-# --------------------------------------
-# Rhythmbox Merge Plays from Last.fm
-# --------------------------------------
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+""" Rhythmbox Merge Plays from Last.fm
+    ----------------Authors----------------
+    Lachlan de Waard <lachlan.00@gmail.com>
+    ----------------Licence----------------
+    GNU General Public License version 3
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 
 import os
 import codecs
